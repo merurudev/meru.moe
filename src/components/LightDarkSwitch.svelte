c53d3da49cb08f062e180e5dfe843f2ec12b0ad3
merurudev <meru_moe@proton.me>
2024-12-21 11:42:35 +0900

    ts-ignore(笑)

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -48,11 +48,13 @@ function toggleScheme() {
 
 function showPanel() {
   const panel = document.querySelector('#light-dark-panel')
+  //@ts-ignore
   panel.classList.remove('float-panel-closed')
 }
 
 function hidePanel() {
   const panel = document.querySelector('#light-dark-panel')
+  //@ts-ignore
   panel.classList.add('float-panel-closed')
 }
 </script>
